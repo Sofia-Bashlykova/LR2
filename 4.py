a38c5313a240811e778d93d2767775b3b8d4e2bc
Sofia-Bashlykova <Sofia.bashlykova14@gmail.com>
2023-05-30 08:12:18 +0300

    add file

---
+++ 4.py
@@ -0,0 +1,15 @@
+from random import randint
+i = 0
+j = 0
+while i < 3:
+    x = randint(1, 100)
+    y = randint(1, 100)
+    z = int(input(str(x) + '+' + str(y) + '='))
+    if z != x + y:
+        print('ответ неверный')
+        i += 1
+    else:
+        print('Правильно!')
+        j += 1
+    if i == 3:
+        print('игра окончена. Правильных ответов:', j)
